d4b7ad5f059a76d803e308baee36640a0caff4a4
YarexUCR <yarexespinoza02@gmail.com>
2025-04-28 10:30:45 -0600

    ajustes

--- src/main/java/Lenguajes/Proyecto1/restController/CategoriaEjercicioRestController.java
+++ src/main/java/Lenguajes/Proyecto1/restController/CategoriaEjercicioRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,8 @@ import Lenguajes.Proyecto1.dto.CategoriaEjercicioDTO;
 import Lenguajes.Proyecto1.mapper.CategoriaEjercicioMapper;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") // Permite CORS solo para este controlador
+
 @RequestMapping("/api/categorias-ejercicio")
 public class CategoriaEjercicioRestController  {
 
